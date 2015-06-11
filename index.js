0e2feefc232139b5fe336b39c771c5d28193a69b
kaflan <kaf_90@mail.ru>
2015-06-12 02:02:41 +0300

    all func

--- index.js
+++ index.js
@@ -3,28 +3,38 @@
  */
 window.addEventListener('load', function load() {
   'use strict';
-  var i;
   var xmlReqvest = new XMLHttpRequest();
   var span = document.querySelectorAll('span');
   xmlReqvest.open('GET', 'https://cors-test.appspot.com/test', true);
-  xmlReqvest.addEventListener('readystatechange', function hand() {
+  xmlReqvest.addEventListener('readystatechange', function hand1() {
     if (xmlReqvest.readyState === xmlReqvest.DONE && xmlReqvest.status === 200) {
-      for (i = 0; i < span.length; i++) {
-        span[i].innerHTML = ('Ok').bold();
-        span[i].style.color = 'green';
-
-      }
-      return false;
+      span[0].innerHTML = ('Ok').bold();
+      span[0].style.color = 'green';
+      return true;
     }
-    for (i = 0; i < span.length; i++) {
-      span[i].innerHTML = ('Fail').bold();
-      span[i].style.color = 'red';
-    }
+    span[0].innerHTML = ('Failed').bold();
+    span[0].style.color = 'red';
   });
   xmlReqvest.send();
-
+  xmlReqvest.open('POST', 'https://cors-test.appspot.com/test', true);
+  xmlReqvest.addEventListener('readystatechancge', function hand2(){
+    if (xmlReqvest.readyState === xmlReqvest.DONE && xmlReqvest.status === 200) {
+      span[1].innerHTML = ('Ok').bold();
+      span[1].style.color = 'green';
+      return true;
+    }
+    span[0].innerHTML = ('Failed').bold();
+    span[0].style.color = 'red';
+  });
+  xmlReqvest.send();
+  xmlReqvest.open('WEIRD', 'https://cors-test.appspot.com/test', true);
+  xmlReqvest.addEventListener('readystatechancge', function hand3(){
+    if (xmlReqvest.readyState === xmlReqvest.DONE && xmlReqvest.status === 200) {
+      span[2].innerHTML = ('Ok').bold();
+      span[2].style.color = 'green';
+      return true;
+    }
+    span[2].innerHTML = ('Failed').bold();
+    span[2].style.color = 'red';
+  });
 });
-//spanAll[1].innerText = ('Ok').bold();
-//spanAll[1].style.color = 'green';
-//spanAll[2].innerText = ('Failed').bold();
-//spanAll[2].style.color = 'red';
