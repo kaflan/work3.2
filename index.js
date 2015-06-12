ba7bafa57c783d7d7fd32cce6523b7002d09516c
kaflan <kaf_90@mail.ru>
2015-06-13 02:14:04 +0300

    all func

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 window.addEventListener('load', function load() {
   'use strict';
   var xmlReqvest = new XMLHttpRequest();
+  var xmlReq1 = new XMLHttpRequest();
+  var xmlReq2 = new XMLHttpRequest();
   var span = document.querySelectorAll('span');
   xmlReqvest.open('GET', 'https://cors-test.appspot.com/test', true);
   xmlReqvest.addEventListener('readystatechange', function hand1() {
@@ -14,27 +16,31 @@ window.addEventListener('load', function load() {
     }
     span[0].innerHTML = ('Failed').bold();
     span[0].style.color = 'red';
+    return false;
   });
   xmlReqvest.send();
-  xmlReqvest.open('POST', 'https://cors-test.appspot.com/test', true);
-  xmlReqvest.addEventListener('readystatechancge', function hand2(){
-    if (xmlReqvest.readyState === xmlReqvest.DONE && xmlReqvest.status === 200) {
+  xmlReq1.open('POST', 'https://cors-test.appspot.com/test', true);
+  xmlReq1.addEventListener('readystatechange', function hand2() {
+    if (xmlReq1.readyState === xmlReq1.DONE && xmlReq1.status === 200) {
       span[1].innerHTML = ('Ok').bold();
       span[1].style.color = 'green';
       return true;
     }
-    span[0].innerHTML = ('Failed').bold();
-    span[0].style.color = 'red';
+    span[1].innerHTML = ('Failed').bold();
+    span[1].style.color = 'red';
+    return false;
   });
-  xmlReqvest.send();
-  xmlReqvest.open('WEIRD', 'https://cors-test.appspot.com/test', true);
-  xmlReqvest.addEventListener('readystatechancge', function hand3(){
-    if (xmlReqvest.readyState === xmlReqvest.DONE && xmlReqvest.status === 200) {
+  xmlReq1.send();
+  xmlReq2.open('WEIRD', 'https://cors-test.appspot.com/test', true);
+  xmlReq2.addEventListener('readystatechange', function hand3() {
+    if (xmlReq2.readyState === xmlReq2.DONE && xmlReq2.status === 200) {
       span[2].innerHTML = ('Ok').bold();
       span[2].style.color = 'green';
       return true;
     }
     span[2].innerHTML = ('Failed').bold();
     span[2].style.color = 'red';
+    return true;
   });
+  xmlReq2.send();
 });
