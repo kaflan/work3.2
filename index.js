c8b08a91fadada8af09d4fa59c14246a4689ec61
kaflan <kaf_90@mail.ru>
2015-06-13 02:57:02 +0300

    valid  to ok

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ window.addEventListener('load', function load() {
   var xmlReq1 = new XMLHttpRequest();
   var xmlReq2 = new XMLHttpRequest();
   var span = document.querySelectorAll('span');
+  var valid =  'ok';
   xmlReqvest.open('GET', 'https://cors-test.appspot.com/test', true);
   xmlReqvest.addEventListener('readystatechange', function hand1() {
+    var ok;
     if (xmlReqvest.readyState === xmlReqvest.DONE && xmlReqvest.status === 200) {
-      span[0].innerHTML = ('Ok').bold();
-      span[0].style.color = 'green';
-      return true;
+      ok = JSON.parse(xmlReqvest.responseText);
+      if (ok.status === valid) {
+        span[0].innerHTML = ('Ok').bold();
+        span[0].style.color = 'green';
+        return true;
+      }
     }
     span[0].innerHTML = ('Failed').bold();
     span[0].style.color = 'red';
@@ -21,10 +26,15 @@ window.addEventListener('load', function load() {
   xmlReqvest.send();
   xmlReq1.open('POST', 'https://cors-test.appspot.com/test', true);
   xmlReq1.addEventListener('readystatechange', function hand2() {
+    var ok;
     if (xmlReq1.readyState === xmlReq1.DONE && xmlReq1.status === 200) {
-      span[1].innerHTML = ('Ok').bold();
-      span[1].style.color = 'green';
-      return true;
+      ok = JSON.parse(xmlReq1.responseText);
+      console.log(ok);
+      if (ok.status === valid) {
+        span[1].innerHTML = ('Ok').bold();
+        span[1].style.color = 'green';
+        return true;
+      }
     }
     span[1].innerHTML = ('Failed').bold();
     span[1].style.color = 'red';
@@ -33,10 +43,14 @@ window.addEventListener('load', function load() {
   xmlReq1.send();
   xmlReq2.open('WEIRD', 'https://cors-test.appspot.com/test', true);
   xmlReq2.addEventListener('readystatechange', function hand3() {
+    var ok;
     if (xmlReq2.readyState === xmlReq2.DONE && xmlReq2.status === 200) {
-      span[2].innerHTML = ('Ok').bold();
-      span[2].style.color = 'green';
-      return true;
+      ok = JSON.parse(xmlReq2.responseText);
+      if (ok.status === valid) {
+        span[2].innerHTML = ('Ok').bold();
+        span[2].style.color = 'green';
+        return true;
+      }
     }
     span[2].innerHTML = ('Failed').bold();
     span[2].style.color = 'red';
